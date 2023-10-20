907559a1682592cafb003558558c1a960a516d7d
aicha <aicha70tabite@gmail.com>
2023-10-20 10:06:06 +0100

    add toggle for category at index

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-function toggleText(textId) {
-    var textDiv = document.getElementById('text' + textId);
-    if (textDiv.style.display === 'none') {
-        textDiv.style.display = 'block';
-    } else {
-        textDiv.style.display = 'none';
-    }
-}
 const nav = document.getElementById("menu");
 const searchIcon = document.getElementById("searchIcon");
 const navOpenBtn = document.getElementById("navOpenBtn");
 const navCloseBtn = document.querySelector(".navCloseBtn");
+const accessoriesList = document.getElementById("accessoriesList");
+const clickToShowList = document.getElementById("clickToShowList");
+
+clickToShowList.addEventListener("click", function () {
+  if (
+    accessoriesList.style.display === "none" ||
+    accessoriesList.style.display === ""
+  ) {
+    accessoriesList.style.display = "block";
+  } else {
+    accessoriesList.style.display = "none";
+  }
+});
 
 searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if (nav.classList.contains("openSearch")) {
-        searchIcon.classList.replace("uil-search", "uil-times");
-    } else {
-        searchIcon.classList.replace("uil-times", "uil-search");
-    }
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    searchIcon.classList.replace("uil-search", "uil-times");
+  } else {
+    searchIcon.classList.replace("uil-times", "uil-search");
+  }
 });
 
 navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("uil-times", "uil-search");
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
 });
 
 navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
+  nav.classList.remove("openNav");
 });
